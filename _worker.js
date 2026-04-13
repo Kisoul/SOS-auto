aba09a6780118932c4e4aed0e77b0deb88dbfce2
Kisoul <32985553+Kisoul@users.noreply.github.com>
2026-04-13 15:03:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1005,8 +1005,8 @@ function generateHomePage(scuValue) {
 
       <div class="form-group">
         <label>自定义端口（可选）</label>
-        <input type="text" id="customPorts" placeholder="例如：443系端口443,2053,2083,2087,2096,8443；80系端口80,8080,8880,2052,2082,2086,2095">
-        <small>支持英文逗号分隔；留空则使用默认端口策略</small>
+        <input type="text" id="customPorts" placeholder="留空则使用443端口">
+        <small>支持英文逗号分隔；例如：TLS端口：443,2053,2083,2087,2096,8443非TLS端口：80,8080,8880,2052,2082,2086,2095</small>
       </div>
 
       <div class="list-item" onclick="toggleSwitch('switchDomain')">
